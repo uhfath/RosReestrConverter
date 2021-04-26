070f1959c7cc98a7c0bbc0e8afa582a6b9539043
Kasbolat Kumakhov <uhfath@gmail.com>
2021-04-26 17:39:34 +0300

    Добавлен вывод лога в файл Добавлено переименование выходных файлов для избежания перезаписи (добавляется индекс)

--- RosReestrConverter/Program.cs
+++ RosReestrConverter/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml.Linq;
 using System.Xml.XPath;
@@ -12,6 +13,9 @@ namespace RosReestrConverter
 {
 	internal static class Program
 	{
+		private const string LogFileName = "rosreestr.log";
+		private const string UniqueFileNameTemplate = "{0}_({1}){2}";
+
 		private static readonly string InvalidChars = new(
 			Path.GetInvalidPathChars()
 				.Concat(Path.GetInvalidFileNameChars())
@@ -27,9 +31,30 @@ namespace RosReestrConverter
 			"/extract_base_params_build/build_record/address_location/address/readable_address",
 		};
 
+		private static TextWriter consoleOut;
+
+		private static void InitLog()
+		{
+			consoleOut = Console.Out;
+
+			var writer = new StreamWriter(File.Create(LogFileName))
+			{
+				AutoFlush = true,
+			};
+
+			Console.SetOut(writer);
+		}
+
+		private static void Log(string message, params object[] parameters)
+		{
+			var text = string.Format(message, parameters);
+			consoleOut.WriteLine(text);
+			Console.WriteLine("{0}\t{1}", DateTime.Now, text);
+		}
+
 		private static string GetNameFromXML(string file, ZipArchiveEntry entry)
 		{
-			Console.WriteLine("Получение имени: {0}", entry.Name);
+			Log("Получение имени: {0}", entry.Name);
 
 			using var stream = entry.Open();
 			var document = XDocument.Load(stream);
@@ -41,37 +66,61 @@ namespace RosReestrConverter
 
 			if (string.IsNullOrWhiteSpace(name))
 			{
-				Console.Error.WriteLine("Не найден тэг с именем");
+				Log("Не найден тэг с именем");
 				name = Path.GetFileName(file) + "_" + entry.Name;
 			}
 
 			return CleanerRegex.Replace(name, string.Empty);
 		}
 
+		private static string EnsureUniqueFileName(string filename)
+		{
+			var initial = filename;
+			var index = 1;
+			while (File.Exists(filename))
+			{
+				var name = string.Format(UniqueFileNameTemplate, Path.GetFileNameWithoutExtension(initial), index, Path.GetExtension(initial));
+				filename = Path.Combine(Path.GetDirectoryName(initial), name);
+				++index;
+			}
+
+			return filename;
+		}
+
+		static Program()
+		{
+			InitLog();
+		}
+
 		private static void Main(string[] args)
 		{
 			var files = args
-				.Where(f => string.Equals(Path.GetExtension(f), ".zip", StringComparison.OrdinalIgnoreCase));
+				.Where(f => Directory.Exists(f))
+				.SelectMany(f => Directory.EnumerateFiles(f, "*.zip", SearchOption.AllDirectories))
+				.Concat(args
+					.Where(f => File.Exists(f))
+					.Where(f => string.Equals(Path.GetExtension(f), ".zip", StringComparison.OrdinalIgnoreCase)));
 
-			Console.WriteLine("Всего файлов: {0}", files.Count());
+			Log("Всего файлов: {0}", files.Count());
 
 			foreach (var file in files)
 			{
-				Console.WriteLine("Обработка: {0}", Path.GetFileNameWithoutExtension(file));
+				Log("Обработка: {0}", Path.GetFileNameWithoutExtension(file));
 
 				using var archive = ZipFile.OpenRead(file);
 
 				var xmlEntry = archive.Entries
 					.Single(e => string.Equals(Path.GetExtension(e.Name), ".xml", StringComparison.OrdinalIgnoreCase));
 
-				var pdfName = GetNameFromXML(file, xmlEntry);
-
-				Console.WriteLine("Итоговое имя файла: {0}", pdfName);
-
 				var pdfEntry = archive.Entries
 					.Single(e => string.Equals(Path.GetExtension(e.Name), ".pdf", StringComparison.OrdinalIgnoreCase));
 
-				pdfEntry.ExtractToFile(Path.ChangeExtension(pdfName, ".pdf"), true);
+				var pdfName = GetNameFromXML(file, xmlEntry);
+				var output = EnsureUniqueFileName(pdfName + ".pdf");
+
+				Log("Итоговое имя файла: {0}", pdfName);
+
+				pdfEntry.ExtractToFile(output, true);
 			}
 		}
 	}
